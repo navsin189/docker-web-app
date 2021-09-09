2e969980b769fbb48b1fb53b612f48a972d3f6ce
naveen <navsin189@gmail.com>
2021-09-10 01:10:10 +0530

    update

--- src/components/core/navbar.js
+++ src/components/core/navbar.js
@@ -1,19 +1,30 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "../../components_css/core_css/navbar.css";
-import {makeStyles} from '@material-ui/core/styles';
+import { makeStyles } from "@material-ui/core/styles";
 const NavBar = () => {
   const useStyles = makeStyles({
-    list:{color:"white",textDecoration:"none",margin:"0 10px 0 0"}
+    list: {
+      color: "white",
+      textDecoration: "none",
+      marginRight: "10px",
+      marginBottom: "5px"
+    }
   });
   const styles = useStyles();
   return (
     <div id="navbar-layout">
       <nav>
         <ul>
-          <Link to="/" className={styles.list}>Home</Link>
-          <Link to="/login" className={styles.list}>Login</Link>
-          <Link to="/signup" className={styles.list}>SignUp</Link>
+          <Link to="/" className={styles.list}>
+            Home
+          </Link>
+          <Link to="/login" className={styles.list}>
+            Login
+          </Link>
+          <Link to="/signup" className={styles.list}>
+            SignUp
+          </Link>
         </ul>
       </nav>
     </div>
